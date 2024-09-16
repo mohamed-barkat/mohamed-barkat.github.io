bbacbe4e83a666af7c672492d33b4aab02ef024d
unknown <eng.mohamed.br@gmail.com>
2024-09-16 06:46:38 +0200

    add up button scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-var logo=$("#logo");
+let span = document.querySelector('.up');
+window.onscroll=function()
+{
+    console.log(this.scrollY);
+if(this.scrollY > 500)
+{
+    span.classList.add("show");
+}
+else
+{
+    span.classList.remove("show");
+}
+}
+span.onclick = function()
+{
+window.scrollTo({
+    top:0,
+    behavior:"smooth"
+});
+}
 
+
+
+
+
+var logo=$("#logo");
 
 logo.on('click',()=>{
 
